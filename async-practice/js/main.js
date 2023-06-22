f5416964addd9494242ad977aaf0b0d0fe7f6b8a
tertius Jubber <tjubber@gmail.com>
2023-06-22 11:30:10 +0200

    2

--- async-practice/js/main.js
+++ async-practice/js/main.js
@@ -135,10 +135,10 @@
 // getPaid()
 
 //Code 08
-// async function getACuteDogPhoto(){
-//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
-//     const data = await res.json()
-//     console.log(data)
-// }
-// getACuteDogPhoto()
+async function getACuteDogPhoto(){
+    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await res.json()
+    console.log(data)
+}
+getACuteDogPhoto()
 
